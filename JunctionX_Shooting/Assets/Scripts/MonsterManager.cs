a1798504a3108f5890eda1c887fe1b5f2f0eaba6
minie12 <30361753+minie12@users.noreply.github.com>
2022-04-03 19:17:47 +0900

    Change viewport to 1024x640

---
+++ JunctionX_Shooting/Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+    public GameObject monster;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(monster, RandomPos(), Quaternion.identity);
+        }
+    }
+
+    Vector3 RandomPos()
+    {
+        Vector3 min = new Vector3(-5, -4.5f, 0);
+        Vector3 max = new Vector3(1.2f, 1.5f, 0);
+        Vector3 myVector = new Vector3(UnityEngine.Random.Range(min.x, max.x), UnityEngine.Random.Range(min.y, max.y), UnityEngine.Random.Range(min.z, max.z));
+        return myVector;
+    }
+}
---
+++ JunctionX_Shooting/Assets/Scripts/Start_BTN.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Start_BTN : MonoBehaviour
+{
+    
+    public GameObject start_mon, tutorial_mon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_mon.gameObject.SetActive(false);
+        tutorial_mon.gameObject.SetActive(false);
+    }
+
+    public void StartMouseOn()
+    {
+        //Debug.Log("들어옴");
+        start_mon.SetActive(true);
+    }
+    public void TutorialMouseOn()
+    {
+        tutorial_mon.SetActive(true);
+    }
+
+    public void UIOff()
+    {
+        start_mon.gameObject.SetActive(false);
+        tutorial_mon.gameObject.SetActive(false);
+    }
+
+    public void goPlay()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void goTutorial()
+    {
+        SceneManager.LoadScene("Rank");
+    }
+
+
+
+}
