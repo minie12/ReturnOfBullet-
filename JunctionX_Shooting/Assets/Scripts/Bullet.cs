77ba30a27bb7db6b3f33d6454142385ea613c3c8
solidcellaMoon <solidcella@gmail.com>
2022-06-19 16:24:34 +0900

    벽에 닿았을 때 진동 현상 수정

--- JunctionX_Shooting/Assets/Scripts/Bullet.cs
+++ JunctionX_Shooting/Assets/Scripts/Bullet.cs
@@ -19,7 +19,6 @@ public class Bullet : MonoBehaviour
         wall_south = GameObject.Find("wall_south").GetComponent<Transform>();
         wall_north = GameObject.Find("wall_north").GetComponent<Transform>();
 
-
         // player = GameObject.Find("Player").GetComponent<Transform>();
     }
 
@@ -33,29 +32,33 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // transform.Translate(Vector3.up*0.35f);
-        // transform.Translate(Vector3.up*0.2f);
 
-        // Vector3 direction = new Vector3(0,1,0);
-
-        transform.Translate(direction * 0.35f);
-
-        // if (isFirst)  transform.Translate(direction*0.2f);
-        // else  transform.Translate(Vector3.left*0.2f);
+        transform.Translate(direction * 0.5f);
 
     }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
-        {
-            EnterPos = gameObject.transform.position;
-        }
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
+    //     {
+    //         EnterPos = gameObject.transform.position;
+    //     }
 
+    // }
+
+    void OnCollisionEnter(Collision collision){
+        Test();
+    }
+
+    void Test(){
+        Debug.Log("ok!!!!");
     }
 
     public void OnTriggerExit2D(Collider2D other)
     {
+
+        if (other.gameObject.CompareTag("wall")) Debug.Log("ok");
+
         if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             ExitPos = gameObject.transform.position;
@@ -63,56 +66,11 @@ public class Bullet : MonoBehaviour
             if (isFirst)  //처음 부딪힌 경우
             {
                 isFirst = false;  //이젠 더이상 첫번째가 아니라고 함
-
-                //각도 계산
-                // float angle = (ExitPos.x - EnterPos.x) / (ExitPos.y - EnterPos.y);
-                // float dx = (ExitPos.x - EnterPos.x);
-
-                // float dy = ExitPos.y - EnterPos.y;
-                // double radian = Math.Atan(dx / dy);
-                // float distance;
-
-                // switch (other.gameObject.tag)
-                // {
-                //     //next bullet의 시작지점 정하기 
-                //     case "east":
-                //         // distance = wall_east.transform.position.y - EnterPos.y;
-                //         // NextPos = new Vector2(0, 0);
-                //         NextPos = new Vector2(ExitPos.x, ExitPos.y);
-
-                //         if(ExitPos.y < wall_east.transform.position.y) direction = Vector3.right;
-                //         else direction = Vector3.left;
-
-                //         // NextPos = new Vector2(wall_west.transform.position.x, wall_west.transform.position.y + distance);
-                //         Debug.Log("Modified East");
-                //         break;
-
-                //     case "west":
-                //         distance = wall_west.transform.position.y - EnterPos.y;
-                //         NextPos = new Vector2(wall_east.transform.position.x, wall_east.transform.position.y + distance);
-                //         // Debug.Log("West to East");
-                //         break;
-
-                //     case "north":
-                //         distance = wall_north.transform.position.x - EnterPos.x;
-                //         NextPos = new Vector2(wall_south.transform.position.x + distance, wall_south.transform.position.y);
-                //         // Debug.Log("North to South");
-                //         break;
-
-                //     case "south":
-                //         distance = wall_south.transform.position.x - EnterPos.x;
-                //         NextPos = new Vector2(wall_north.transform.position.x + distance, wall_north.transform.position.y);
-                //         // Debug.Log("South to North");
-                //         break;
-                // }
-
-                // gameObject.transform.position = NextPos;
-
-
-
                 direction = Vector3.down;
             }
+
         }
-        else if (other.gameObject.CompareTag("outWall")) Destroy(gameObject);
+        else if (other.gameObject.CompareTag("wall")) Destroy(gameObject);
+
     }
 }
